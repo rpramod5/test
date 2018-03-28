afe037d3487470d15b51b6f8076a4ffba4539659
rpramodd <32128766+rpramodd@users.noreply.github.com>
2018-03-28 12:33:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const os = require('os');
+const hostname=os.hostname();
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send(hostname))
 
 app.listen(8080, () => console.log('Example app listening on port 8080!'))
